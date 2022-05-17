5e0aae7606d03ceb547f2c8ba4e489519635a3f1
Amarendra9031 <amarendrakumar9031@gmail.com>
2022-05-17 16:43:03 +0530

    Pradeeps commit

---
+++ src/test/java/Pradeep/createOrg.java
@@ -0,0 +1,11 @@
+package Pradeep;
+
+public class createOrg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new org created by pradeep");
+
+	}
+
+}
